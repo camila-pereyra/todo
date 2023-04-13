729babaa2dab801e27ddbb19a1942553e95bef14
camila.pereyra <campereyra23051995@gmail.com>
2023-04-12 22:27:18 -0300

    "Config predeterminada"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Container from "./components/Container/Container";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Container />
     </div>
   );
-}
+};
 
 export default App;
